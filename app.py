ca99031bfff69f10b85fd2175e423c3a2b2c668b
naveedhaq02 <naveedhaq@tamu.edu>
2023-11-11 10:25:41 -0600

    db json file

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 import csv
 import json
 
-csvfile = "StudentsPerformance.csv"
-json_file_path = 'db.json'
+# Replace 'your_file.csv' with your CSV file name
+csv_file_path = 'StudentsPerformance.csv'
+json_file_path = 'db1.json'
 
+# Function to convert values to integers where applicable
+def convert_to_int(row):
+    new_row = {}
+    for key, value in row.items():
+        if key != 'STUDENT ID':
+            try:
+                new_row[key] = int(value)
+            except ValueError:
+                new_row[key] = value
+        else:
+            new_row[key] = value
+    return new_row
+
+# Read the CSV file and convert to JSON
 data = []
-with open(csvfile, mode='r', encoding='utf-8') as csvfile:
+with open(csv_file_path, mode='r', encoding='utf-8') as csvfile:
     csv_reader = csv.DictReader(csvfile)
     for row in csv_reader:
-        data.append(row)
+        data.append(convert_to_int(row))
 
 # Write the JSON data to db.json
 with open(json_file_path, mode='w', encoding='utf-8') as jsonfile:
