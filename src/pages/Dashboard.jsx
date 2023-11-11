785f5f9ee02b3ce2503a15beb75c3e83dc28802c
JoshHare <jmhhare@gmail.com>
2023-11-11 14:53:28 -0600

    kk

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
-export default function Dashboard() {
+// Dashboard.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <h2>Dashboard</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/predict">Predict</Link>
+          </li>
+          <li>
+            <Link to="/create">Chi Squared</Link> 
+          </li>
+          {/* Add other navigation links as needed */}
+        </ul>
+      </nav>
+      {/* Your Dashboard component content */}
+    </div>
+  );
+};
+
+export default Dashboard;
